9a1aced4eba88aa625a77a8087e540b5e5d1af3b
Amadeu Andrade <amadeuandrade@amadeus-macbook-pro.local>
2016-09-26 19:38:10 +0100

    Initial Commit

---
+++ bliss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bliss
+//
+//  Created by Amadeu Andrade on 26/09/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
