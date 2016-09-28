59e18d5a28dc51bb82cd5647f5ef7ed00595f289
Amadeu Andrade <amadeuandrade@Amadeus-MacBook-Pro.local>
2016-09-28 19:33:20 +0100

    ADDED: Grab the first 10 questions

--- bliss/QuestionsVC.swift
+++ bliss/QuestionsVC.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Alamofire
 
 class QuestionsVC: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
 
   // MARK: - PROPERTIES
   
-  var actualOffset = 0
+  var currentOffsetParameter = 0
+  let limitParameter = 10
+  let APIBaseURL = "https://private-anon-d0ef2ee091-blissrecruitmentapi.apiary-mock.com/questions?"
   var textToSearch: String?
   var searchIsActive = false
   var questions = [Question]()
@@ -35,44 +38,24 @@ class QuestionsVC: UIViewController, UISearchBarDelegate, UITableViewDelegate, U
     // Button to hide/show search bar
     let searchButton = UIBarButtonItem(image: UIImage(named: "search"), style: .Plain, target: self, action: #selector(QuestionsVC.updateSearchBarVisibility))
     self.navigationItem.rightBarButtonItem = searchButton
-    
-    // Test data:
-    let info = [
-      "id": 10,
-      "question": "Favourite programming language?",
-      "image_url": "https://dummyimage.com/600x400/000/fff.png&text=question+1+image+(600x400)",
-      "thumb_url": "https://dummyimage.com/120x120/000/fff.png&text=question+1+image+(120x120)",
-      "published_at": "2015-08-05T08:40:51.620Z",
-      "choices": [
-        ["choice": "Swift","votes": 2048],
-        ["choice": "Python","votes": 1024],
-        ["choice": "Objective-C","votes": 512],
-        ["choice": "Ruby","votes": 256]
-      ]
+    // Grab the first questions
+    let parametersToURL = [
+      "limit": limitParameter,
+      "offset": currentOffsetParameter
     ]
-    
-    let info2 = [
-      "id": 1,
-      "question": "filter me",
-      "image_url": "https://dummyimage.com/600x400/000/fff.png&text=question+1+image+(600x400)",
-      "thumb_url": "https://dummyimage.com/120x120/000/fff.png&text=question+1+image+(120x120)",
-      "published_at": "2015-08-05T08:40:51.620Z",
-      "choices": [
-        ["choice": "Swift","votes": 2048],
-        ["choice": "Python","votes": 1024],
-        ["choice": "Objective-C","votes": 512],
-        ["choice": "Ruby","votes": 256]
-      ]
-    ]
+    Alamofire.request(.GET, APIBaseURL, parameters: parametersToURL, encoding: ParameterEncoding.URL, headers: nil).responseJSON { (response: Response<AnyObject, NSError>) in
+      if let result = response.result.value as? [[String: AnyObject]] {
+        for questionEntry in result {
+          let question = Question(questionInfo: questionEntry)
+          self.questions.append(question)
+        }
+        self.tableView.reloadData()
+        self.currentOffsetParameter += self.limitParameter
+      } else {
+        //error || nothing to show
+      }
+    }
 
-    let q1 = Question(questionInfo: info)
-    let q2 = Question(questionInfo: info)
-    let q3 = Question(questionInfo: info)
-    let q4 = Question(questionInfo: info2)
-    questions.append(q1)
-    questions.append(q2)
-    questions.append(q3)
-    questions.append(q4)
   }
   
   override func viewDidAppear(animated: Bool) {
